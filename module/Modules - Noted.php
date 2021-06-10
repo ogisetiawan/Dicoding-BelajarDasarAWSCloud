3489d4e3a8facce3e89ad7a25c96e1ba0512f6e1
ogisetiawan <ogisetiawan21@gmail.com>
2021-06-10 16:25:16 +0700

    docs: Pengantar Amazon Web Service (AWS)

---
+++ module/Modules - Noted.php
@@ -0,0 +1,25 @@
+<?php
+//@ PENGANTAR AMAZON WEB SERVICE
+//! Layanan AWS : Komputasi, Penyimpanan dan Keamanan Jaringan
+//?~ Komputasi ( Computasi-Cloud )
+//? Penggunaan sesuai kebutuhan (on-demand) : Contoh jika tiba2 perlu server tinggal klik, atau perlu memori lebih tinggal klik
+//? Submer daya IT : jadi ada layanan yang dibantu seperti Instalasi OS, Update software dll
+//? Melalui Internet : dapat akses melalui internet , web / terprogram
+
+//? Contoh : Suatu perusahaan telah berhasil membangun data center untuk menopang kebutuhan website-nya.
+//? Setiap hari website tersebut rata-rata diakses oleh 10.000 pengunjung, namun umumnya pada hari minggu jumlahnya hanya 5.000 saja. 
+//? Angka ini berubah lagi di hari-hari spesial seperti malam tahun baru ketika tiba-tiba 30.000 pengunjung membanjirinya.
+//? Lantas website tersebut pun down beberapa kali. si Komputasi kloud ini bisa ngebantu sesuai permintaan
+
+//~ Model Penerepan Komputasi Cloud :
+//? Cloud Based Deployment : Aplikasi/Data berbasis cloud sepenuhnya diterapkan di cloud dan semua bagian aplikasi berjalan di cloud.
+//? On-premises Deployment ( Lokal ) : Aplikasi/Data yang diterarpkan di private-cloud ( local )
+//? Hybrid Deployment : Penggabungan Aplikasi/Data berbasis cloud dan Aplikasi/Data tidak ada di cloud
+
+//~ Manfaat dari Komputasi Cloud
+//? Mengubah pengeluaran di muka menjadi pengeluaran varible ( membayar untuk sumber daya yang dikonsumsi saja )
+//? Menghentikan biaya pengelolaan dan pemeliharaan data center
+//? Berhenti menebak kapasitas
+//? Manfaatkan skala ekonomi yang masif
+//? Tingkatkan kecepatan dan ketangkasan
+//? Mendunia dalam hitungan menit
